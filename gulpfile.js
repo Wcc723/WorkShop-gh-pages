4c32be5073f89d9926a373fb409ab49c545bffac
Wcc723 <agito723@gmail.com>
2016-01-08 11:11:47 +0800

    add gulp gh-pages

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var webserver = require('gulp-webserver');
+var layout = require('gulp-layout');
+var frontMatter = require('gulp-front-matter');
+
+gulp.task('webserver', function() {
+  gulp.src('./public')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      host: '0.0.0.0',
+      port: 10000,
+    }));
+});
+
+gulp.task('gulp-layout', function() {
+  return gulp.src('./source/**/*.html')
+    .pipe(frontMatter())
+    .pipe(layout(function(file) {
+      return file.frontMatter;
+    }))
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('copy', function(){
+  gulp.src(['./source/**/*.css',
+    './source/**/*.png',
+    './source/**/*.jpg',
+    './source/**/*.svg',
+    './source/**/*.js'])
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch( './source/**/*.html', { interval: 500 }, ['gulp-layout']);
+  gulp.watch( ['./source/**/*.css',
+    './source/**/*.png',
+    './source/**/*.jpg',
+    './source/**/*.svg',
+    './source/**/*.js'], { interval: 500 }, ['copy']);
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./public/**/*')
+    .pipe(ghPages());
+});
+
+
+gulp.task('default', ['webserver','gulp-layout', 'copy', 'watch']);
